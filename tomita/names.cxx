af165578306bfe824df5d29340bf51f5710bf499
Euphe <euphetar@gmail.com>
2016-02-11 18:00:03 +0300

    further progress on making Names detection more precise

--- tomita/names.cxx
+++ tomita/names.cxx
@@ -1,20 +1,11 @@
 #encoding "utf-8"    // сообщаем парсеру о том, в какой кодировке написана грамматика
 #GRAMMAR_ROOT S     // указываем корневой нетерминал грамматики
 
-/*
-PersonName -> Word<h-reg1, rt, gnc-agr[1]>  AnyWord<h-reg1, gnc-agr[1]>  AnyWord<h-reg1, gnc-agr[1]> | AnyWord<h-reg1, gnc-agr[1]>  AnyWord<h-reg1, gnc-agr[1]>  Word<h-reg1, rt, gnc-agr[1]> | Word<h-reg1, rt, gnc-agr[1]>  Word<h-reg1, gnc-agr[1]> | Word<h-reg1, gnc-agr[1]>  Word<h-reg1, rt, gnc-agr[1]> ;
-ProperName -> Word<h-reg2> | Word<h-reg1, gnc-agr[1], rt> Word<h-reg1, gnc-agr[1]>*;// | Word<gnc-agr[1]>* Word<h-reg1, gnc-agr[1], rt>;
-GenericName ->  Word<h-reg1>+;// Имя собственное
-*/
-/*
-GeographicalNamedAfter -> ProperName 'имени' PersonName | ProperName PersonName<gram="род">;
-*/
 GenericName -> Word<h-reg1, gnc-agr[1]> Word<h-reg1,gnc-agr[1]>*;
-PersonName -> Word<h-reg1, rt, gnc-agr[1]>  AnyWord<h-reg1, gnc-agr[1]>  AnyWord<h-reg1, gnc-agr[1]> | AnyWord<h-reg1, gnc-agr[1]>  AnyWord<h-reg1, gnc-agr[1]>  Word<h-reg1, rt, gnc-agr[1]> | Word<h-reg1, rt, gnc-agr[1]>  Word<h-reg1, gnc-agr[1]> | Word<h-reg1, gnc-agr[1]>  Word<h-reg1, rt, gnc-agr[1]> ;
+PersonName -> Word<h-reg1, rt, gnc-agr[1], gram="фам">  AnyWord<h-reg1, gnc-agr[1], gram="имя">  AnyWord<h-reg1, gnc-agr[1], gram="отч"> | AnyWord<h-reg1, gnc-agr[1], gram="фам">  AnyWord<h-reg1, gnc-agr[1], gram="отч">  Word<h-reg1, rt, gnc-agr[1], gram="имя"> | Word<h-reg1, rt, gnc-agr[1], gram="фам">  Word<h-reg1, gnc-agr[1], gram="имя"> | Word<h-reg1, gnc-agr[1], gram="имя">  Word<h-reg1, rt, gnc-agr[1], gram="фам"> ;
 
-Event -> Word<h-reg2> | Noun<h-reg2> | Noun<h-reg1, gnc-agr[1], rt> Word<h-reg1, gnc-agr[1]>*;// | Word<h-reg1, gnc-agr[1]>* Noun<h-reg1, gnc-agr[1], rt>;
+Event -> Word<h-reg2> | Noun<h-reg2> | Noun<h-reg1, gnc-agr[1], rt> Word<h-reg1, gnc-agr[1]>*;
 GeographicalNamedAfter -> Event 'имени' PersonName | Event PersonName<gram="род">;
 OtherNamed -> GeographicalNamedAfter | Event | GenericName;
-S -> OtherNamed | PersonName;// | PersonName | GeographicalNamedAfter ;// | Geographical;//| Geographical | 
+S -> OtherNamed | PersonName;
 
-//FullName -> FirstName<rt, gnc—agr[1]> Word<h—reg1, gnc—agr[1]>* | Word<h—reg1, gnc—agr[1]>* FirstName<rt, gnc—agr[1]> 
