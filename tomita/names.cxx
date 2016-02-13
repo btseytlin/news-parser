c66f496f9f37a1bae15b9740db62a3735fb6e10b
Euphe <kururugisuzakueuphe@ya.ru>
2016-02-13 15:10:51 +0300

    added regex to not capture numbers as names

--- tomita/names.cxx
+++ tomita/names.cxx
@@ -1,11 +1,10 @@
 #encoding "utf-8"    // сообщаем парсеру о том, в какой кодировке написана грамматика
 #GRAMMAR_ROOT S     // указываем корневой нетерминал грамматики
 
-GenericName -> Word<h-reg1, gnc-agr[1]> Word<h-reg1,gnc-agr[1]>*;
-PersonName -> Word<h-reg1, rt, gnc-agr[1], gram="фам">  AnyWord<h-reg1, gnc-agr[1], gram="имя">  AnyWord<h-reg1, gnc-agr[1], gram="отч"> | AnyWord<h-reg1, gnc-agr[1], gram="фам">  AnyWord<h-reg1, gnc-agr[1], gram="отч">  Word<h-reg1, rt, gnc-agr[1], gram="имя"> | Word<h-reg1, rt, gnc-agr[1], gram="фам">  Word<h-reg1, gnc-agr[1], gram="имя"> | Word<h-reg1, gnc-agr[1], gram="имя">  Word<h-reg1, rt, gnc-agr[1], gram="фам"> ;
-
+GenericName -> Word<h-reg1, gram="~NUM">+[gnc-agr];
+PersonName -> Word<h-reg1, rt, gnc-agr[1], gram="famn">  AnyWord<h-reg1, gnc-agr[1], gram="persn">  AnyWord<h-reg1, gnc-agr[1], gram="patrn"> | AnyWord<h-reg1, gnc-agr[1], gram="famn">  AnyWord<h-reg1, gnc-agr[1], gram="patrn">  Word<h-reg1, rt, gnc-agr[1], gram="persn"> | Word<h-reg1, rt, gnc-agr[1], gram="famn">  Word<h-reg1, gnc-agr[1], gram="persn"> | Word<h-reg1, gnc-agr[1], gram="persn">  Word<h-reg1, rt, gnc-agr[1], gram="famn">;
 Event -> Word<h-reg2> | Noun<h-reg2> | Noun<h-reg1, gnc-agr[1], rt> Word<h-reg1, gnc-agr[1]>*;
 GeographicalNamedAfter -> Event 'имени' PersonName | Event PersonName<gram="род">;
 OtherNamed -> GeographicalNamedAfter | Event | GenericName;
-S -> OtherNamed | PersonName;
+S -> OtherNamed<wff=/(\d+[^\d\s\t]+)|([^\d\s\t]+\d+)|([^\d\s]+)/> | PersonName< wff=/(\d+[^\d\s\t]+)|([^\d\s\t]+\d+)|([^\d\s]+)/>;
 
