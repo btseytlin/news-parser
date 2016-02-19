fdcbafb3a988d69655e0c0f6b8ad80aa482b3b5b
Euphe <kururugisuzakueuphe@ya.ru>
2016-02-19 21:09:26 +0300

    more grammatics improvenments

--- tomita/geo.cxx
+++ tomita/geo.cxx
@@ -4,7 +4,7 @@
 Generic -> Word<gram="~V,~A,~ADV,~APRO,~brev,~poss", kwset=~["имена", "стоп_слова"], ~quoted, h-reg1,wff=/([^\s]{4,})/, gnc-agr[1]> Word< kwset=~["имена", "стоп_слова"],~quoted, h-reg1,wff=/([^\s]{4,})/, gnc-agr[1]>*;
 Quoted -> Word<h-reg1, quoted,gnc-agr[1]> Word<quoted,gnc-agr[1]>*;
 Abbr -> AnyWord<gram="abbr">;
-Geog -> Geo | AnyWord<kwtype='названия_стран'>;
+Geog -> Geo;
 CamelCase -> AnyWord<h-reg2>;
 
 Event -> Noun<h-reg1, gnc-agr[1], rt> Adj<l-reg, gnc-agr[1]> Word<l-reg, gram="род" ,gnc-agr[1]>; // Турнир четырех наций
@@ -12,7 +12,7 @@ Event -> Adj<h-reg1, gnc-agr[1]> Noun<gnc-agr[1], rt> Word<gram="~V,~ADV,~A,~ADV
 Event -> Adj<h-reg1, gnc-agr[1]> Noun<gnc-agr[1], rt>;
 GenericName -> Quoted | Abbr | CamelCase;
 
-Location ->  Noun<~quoted, h-reg1, gnc-agr[1], rt> Word<~quoted, h-reg1, gnc-agr[1]>+ | Noun<~quoted, h-reg1, gnc-agr[1]> Word<gram="~V",~quoted, l-reg, gnc-agr[1]>+;
+Location ->  Noun<~quoted, h-reg1, nc-agr[1], rt> Word<~quoted, h-reg1, nc-agr[1]>+ | Noun<~quoted, h-reg1, gnc-agr[1]> Word<gram="~V",~quoted, l-reg, gnc-agr[1]>+;
 Location -> Adj<h-reg1, gnc-agr[1]> AnyWord<kwtype='маркеры_локаций',gnc-agr[1]>;
 Location -> AnyWord<h-reg1, kwtype='маркеры_локаций'> Noun<h-reg1>;
 OtherNamed -> Location<wff=/([^\s]{4,})/> | GenericName | Event<wff=/([^\s]{4,})/> | Generic | Geog;// | GeographicalNamedAfter |  Event;
